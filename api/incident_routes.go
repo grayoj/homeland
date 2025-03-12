60c7a7fbb483b32d87d9d28213ed8154edce95c8
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2025-03-12 22:35:48 +0100

    (feat): added routes for incident

---
+++ api/incident_routes.go
@@ -0,0 +1,12 @@
+package api
+
+import (
+	"homeland/handlers/incident"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/uptrace/bun"
+)
+
+func RegisterIncidentRoutes(r chi.Router, db *bun.DB) {
+	r.Post("/incidents", incident.CreateIncidentHandler(db))
+}
